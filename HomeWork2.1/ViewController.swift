8b26e83df7e90d0824e7475d7bfffbc850c1bb89
Vadim Kislov <kislov.v@gmail.com>
2022-03-25 22:58:30 +0300

    Initial Commit

---
+++ HomeWork2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2.1
+//
+//  Created by Kislov Vadim on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
